66d9eb8813dcdb14457c7bd56119f364d17e15de
Krzysztof Czerenko <krzysztofczerenko@gmail.com>
2025-02-04 21:58:13 +0100

    final i hope

--- project/life_sim.c
+++ project/life_sim.c
@@ -6,23 +6,27 @@
 #define ALIVE 1
 #define DEAD  0
 
-// Globalne zmienne: wymiary planszy i liczba generacji
-int rows, cols, generations;
+// Globalne zmienne
+
+// Wymiary planszy i liczba generacji
+int rows, cols, generations; 
 
 // Tablice 2D reprezentujące stan planszy
 int **current_generation;
 int **next_generation;
 
-// Liczba wątków (pula wątków)
+// Liczba wątków
 int num_workers;
 
 // Zmienne synchronizacyjne – mutex i zmienne warunkowe
-pthread_mutex_t cond_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond_start = PTHREAD_COND_INITIALIZER; // do powiadamiania wątków o rozpoczęciu generacji
-pthread_cond_t cond_done  = PTHREAD_COND_INITIALIZER; // do oczekiwania, aż wszyscy wątki zakończą obliczenia
 
-// Globalne zmienne sterujące symulacją
-int current_gen = 0; // numer bieżącej generacji (zadania)
+pthread_mutex_t cond_mutex = PTHREAD_MUTEX_INITIALIZER; // do zarządzania dostępem do zmiennych globalnych
+
+pthread_cond_t cond_start = PTHREAD_COND_INITIALIZER; // do powiadamiania wątków o rozpoczęciu generacji
+
+pthread_cond_t cond_done  = PTHREAD_COND_INITIALIZER; // do oczekiwania, aż wszystkie wątki skończą działanie
+
+int current_gen = 0; // numer bieżącej generacji
 int done_count = 0;  // licznik wątków, które ukończyły przetwarzanie aktualnej generacji
 
 // Struktura przekazywana do wątku – określa przydział wierszy
@@ -32,7 +36,7 @@ typedef struct {
     int end_row;    // ostatni przetwarzany wiersz (wyłącznie)
 } WorkerData;
 
-// Funkcja pomocnicza: liczy liczbę żywych sąsiadów komórki (r, c)
+// Funkcja pomocnicza licząca liczbę żywych sąsiadów komórki
 int count_alive_neighbors(int r, int c) {
     int count = 0;
     for (int dr = -1; dr <= 1; dr++) {
@@ -50,34 +54,35 @@ int count_alive_neighbors(int r, int c) {
     return count;
 }
 
-// Funkcja wątku-robotnika: każdy wątek przetwarza przypisany fragment planszy
+// Funkcja wątku przetwarzającego przypisany fragment planszy
 void* worker_thread(void *arg) {
     WorkerData *data = (WorkerData*) arg;
     int local_gen = 0; // numer ostatniej przetworzonej generacji przez ten wątek
 
     while (1) {
-        // Czekamy na sygnał rozpoczęcia nowej generacji
+
+        // Oczekiwanie na sygnał rozpoczęcia nowej generacji
         pthread_mutex_lock(&cond_mutex);
         while (current_gen == local_gen) {
             pthread_cond_wait(&cond_start, &cond_mutex);
         }
-        // Jeżeli numer generacji przekroczył liczbę symulowanych – kończymy pracę
+
+        // Zakończenie pracy po przetworzeniu wymaganej liczby generacji
         if (current_gen > generations) {
             pthread_mutex_unlock(&cond_mutex);
             break;
         }
-        // Aktualizujemy lokalny numer generacji
+        // Aktualizacja lokalnej generacji
         local_gen = current_gen;
         pthread_mutex_unlock(&cond_mutex);
 
-        // Przetwarzamy przypisane wiersze dla bieżącej generacji
+        // Przetwarzanie przypisanych wierszy
         for (int r = data->start_row; r < data->end_row; r++) {
             for (int c = 0; c < cols; c++) {
                 int alive_neighbors = count_alive_neighbors(r, c);
                 int current_state = current_generation[r][c];
                 int next_state = current_state;
 
-                // Zasady Gry w Życie:
                 if (current_state == ALIVE) {
                     if (alive_neighbors < 2 || alive_neighbors > 3)
                         next_state = DEAD;
@@ -89,12 +94,12 @@ void* worker_thread(void *arg) {
             }
         }
 
-        // Po zakończeniu obliczeń robotnik zgłasza ukończenie zadania:
+        // Zgłoszenie ukończenia obliczeń
         pthread_mutex_lock(&cond_mutex);
         done_count++;
         if (done_count == num_workers) {
-            // Jeśli to był ostatni wątek – powiadamiamy główny wątek
-            pthread_cond_signal(&cond_done);
+            // Ostatni wątek ktory skończy prace informuje o tym wątek głowny
+            pthread_cond_signal(&cond_done); 
         }
         pthread_mutex_unlock(&cond_mutex);
     }
@@ -103,11 +108,12 @@ void* worker_thread(void *arg) {
     return NULL;
 }
 
-// Funkcja wyświetlająca planszę w konsoli (ASCII)
+// Funkcja wyświetlająca planszę w konsoli
 void print_board(int **board, int rows, int cols) {
     for (int r = 0; r < rows; r++) {
         for (int c = 0; c < cols; c++) {
             printf("%c", board[r][c] == ALIVE ? 'O' : '.');
+            printf(" ");
         }
         printf("\n");
     }
@@ -118,7 +124,8 @@ void print_board(int **board, int rows, int cols) {
 void write_board_to_file(FILE *fp, int **board, int rows, int cols) {
     for (int r = 0; r < rows; r++) {
         for (int c = 0; c < cols; c++) {
-            fputc(board[r][c] == ALIVE ? '1' : '.', fp);
+            fputc(board[r][c] == ALIVE ? '0' : '.', fp);
+            fputc(' ', fp);
         }
         fputc('\n', fp);
     }
@@ -137,7 +144,6 @@ int main(int argc, char *argv[]) {
     cols        = atoi(argv[4]);
     generations = atoi(argv[5]);
 
-    // Alokacja pamięci dla planszy (tablice 2D)
     current_generation = malloc(rows * sizeof(int*));
     next_generation    = malloc(rows * sizeof(int*));
     for (int i = 0; i < rows; i++) {
@@ -160,7 +166,6 @@ int main(int argc, char *argv[]) {
             }
             current_generation[r][c] = (ch == '1') ? ALIVE : DEAD;
         }
-        // Odczytujemy znak nowej linii (jeśli występuje)
         fgetc(fin);
     }
     fclose(fin);
@@ -178,16 +183,16 @@ int main(int argc, char *argv[]) {
     fprintf(fout, "Generacja 0:\n");
     write_board_to_file(fout, current_generation, rows, cols);
 
-    // Ustalamy liczbę wątków – możemy użyć liczby rdzeni systemowych
+    // Ustalenie liczby wątków
     num_workers = sysconf(_SC_NPROCESSORS_ONLN);
     if (num_workers < 1)
         num_workers = 1;
     if (num_workers > rows)
-        num_workers = rows; // nie tworzymy więcej wątków niż wierszy
+        num_workers = rows;
 
     pthread_t *threads = malloc(num_workers * sizeof(pthread_t));
 
-    // Dzielimy planszę – każdemu wątkowi przydzielamy pewien przedział wierszy
+    // Podział planszy pomiędzy wątki
     int rows_per_worker = rows / num_workers;
     int remainder = rows % num_workers;
     int start = 0;
@@ -201,43 +206,44 @@ int main(int argc, char *argv[]) {
         pthread_create(&threads[i], NULL, worker_thread, wd);
     }
 
-    // Główna pętla symulacji – przetwarzamy kolejne generacje
+    // Główna pętla symulacji
     for (int gen = 1; gen <= generations; gen++) {
-        // Rozpoczynamy nową generację: aktualizujemy globalną zmienną i powiadamiamy wątki
+
+        //Rozpoczęcie nowej generacji i wysłanie sygnału do wątkow
         pthread_mutex_lock(&cond_mutex);
         current_gen = gen;
         pthread_cond_broadcast(&cond_start);
-        // Czekamy, aż wszystkie wątki zakończą obliczenia
+
+        // Oczekiwanie na wszystki wątki
         while (done_count < num_workers) {
             pthread_cond_wait(&cond_done, &cond_mutex);
         }
-        // Resetujemy licznik ukończonych wątków dla kolejnej generacji
+
         done_count = 0;
         pthread_mutex_unlock(&cond_mutex);
 
-        // Po zakończeniu obliczeń kopiujemy bufor next_generation do current_generation
+        // Kopiowanie planszy
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 current_generation[r][c] = next_generation[r][c];
             }
         }
 
-        // Wyświetlamy oraz zapisujemy stan planszy
         printf("Generacja %d:\n", gen);
         print_board(current_generation, rows, cols);
         fprintf(fout, "Generacja %d:\n", gen);
         write_board_to_file(fout, current_generation, rows, cols);
 
-        usleep(200000); // opcjonalna pauza 0.2 sekundy
+        usleep(200000);
     }
 
-    // Sygnalizujemy wątkom zakończenie pracy (numer generacji przekroczony)
+
     pthread_mutex_lock(&cond_mutex);
     current_gen = generations + 1;
     pthread_cond_broadcast(&cond_start);
     pthread_mutex_unlock(&cond_mutex);
 
-    // Czekamy na zakończenie wątków
+    // Oczekiwanie na zakończenie pracy wątkow
     for (int i = 0; i < num_workers; i++) {
         pthread_join(threads[i], NULL);
     }
